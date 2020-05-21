aec51c67cbcc5ab083e2b131567f62fc0b6d72df
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-21 18:15:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def greet_characters(array)
   array.each do |index|
     puts "Hello #{index}!"
 end
+end
 
 def list_dwarves(array)
   #array.each do |ind|
@@ -11,4 +12,5 @@ def list_dwarves(array)
  array.each_with_index do |index|
    
    puts index
+end
 end
