cbb16840add5e9f1ec46305eea46339a83febfbd
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-21 18:05:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |index|
+    puts "Hello #{index}!"
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  array.each 
+ array.each_with_index do |index|
+   array[index]=
+   puts index
 end
